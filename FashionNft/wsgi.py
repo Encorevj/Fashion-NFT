7b2593ff607f85748e24351668ce6a025d8e8a63
Vaibhav <githubvaibhav@gmail.com>
2022-04-22 20:53:17 +0530

    Uploading UI on django

---
+++ FashionNft/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for FashionNft project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FashionNft.settings')
+
+application = get_wsgi_application()
---
+++ resources/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+
+class Genesis(models.Model):
+    original=models.FileField()
